62518b1923ab9255ed01df3383a22f768c9f7db8
sherry <sherryja@163.com>
2026-06-15 11:52:22 +0800

    【stock_barcode_lite】模块，关于整托出库前端预设，及文件结构调整

--- mymodules/stock_barcode_lite/static/src/js/main.js
+++ mymodules/stock_barcode_lite/static/src/js/main.js
@@ -1,9 +1,10 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Homepage } from "./components/homepage";
-import { InboundFlow } from "./components/inbound_flow";
-import { OutboundFlow } from "./components/outbound_flow";
+import { Homepage } from "./homepage";
+import { InboundFlow } from "./inbound_flow";
+import { OutboundFlow } from "./outbound_flow";
+import { WholePalletOutboundPage } from "./whole_outbound";
 
 class InboundPage extends InboundFlow {
     static template = "stock_barcode_lite.InboundPage";
@@ -13,9 +14,14 @@ class OutboundPage extends OutboundFlow {
     static template = "stock_barcode_lite.OutboundPage";
 }
 
+class WholeOutboundPage extends WholePalletOutboundPage {
+    static template = "stock_barcode_lite.WholePalletOutboundPage";
+}
+
 // 组件绑定事件
 registry.category("actions").add("stock_barcode_lite_homepage", Homepage);
 registry.category("actions").add("stock_barcode_lite_inbound", InboundPage);
 registry.category("actions").add("stock_barcode_lite_outbound", OutboundPage);
+registry.category("actions").add("stock_barcode_lite_outbound_whole", WholeOutboundPage);
 
 console.log('[stock_barcode_lite] All pages registered');
