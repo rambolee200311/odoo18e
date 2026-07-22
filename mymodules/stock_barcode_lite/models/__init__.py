d4bc4ec47c358fe2792fb40964f0b94818e8dde7
roger <lj9638520741@163.com>
2026-07-22 11:29:38 +0800

    货位导入中间表逻辑

--- mymodules/stock_barcode_lite/models/__init__.py
+++ mymodules/stock_barcode_lite/models/__init__.py
@@ -7,10 +7,12 @@ from . import outbound_order_sunrise_status
 from . import sunrise_api
 from . import sunrise_product_master_import
 from . import sunrise_order_import
+from . import sunrise_stock_report
 from . import stock_quant_package
 from . import stock_barcode_lite_scan_service
 from . import stock_barcode_lite_outgoing_scan_service
 from . import stock_move_line_inherit
+from . import move_line_location_import
 from . import stock_picking_inherit
 from . import stock_return_picking_inherit
 from . import project_project_inherit
